46fa26a08bee75ce3e2c2c8751af470f735747ac
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-03 20:47:57 +0530

    Added 'Navbar' component & added 'main' tag around children part

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/layout";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,7 +25,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
-                {children}
+                <Navbar />  // Navbar Component
+
+                <main>
+                    {children}
+                </main>
             </body>
         </html>
     );
