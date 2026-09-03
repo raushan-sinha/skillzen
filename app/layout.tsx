aae99d3e4882fb45e3677a1471422dcf7791ce84
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-03 21:10:03 +0530

    Comment updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
-                <Navbar />  // Navbar Component
+                {/* Navbar Component */}
+                <Navbar /> 
 
                 <main>
                     {children}
