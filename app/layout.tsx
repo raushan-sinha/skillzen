bc2314a8b4a6e79c0b8369f0cbbff0a8878772b9
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-06 15:50:21 +0530

    Added new compoennt for Navbar - ResponsiveNavbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout";
+import { ResponsiveNavbar } from "@/components/layout";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         >
             <body className="min-h-full flex flex-col">
                 {/* Navbar Component */}
-                <Navbar /> 
+                <ResponsiveNavbar />
 
                 <main>
                     {children}
