52403144d9a86f823ae2ccf27845ae57f7649b55
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-11 14:47:59 +0530

    Updated new images path

--- data/whyChooseData.data.ts
+++ data/whyChooseData.data.ts
@@ -3,28 +3,28 @@ import { WhyChooseItem } from "@/types/whyChooseDataType.types";
 export const whyChooseData: WhyChooseItem[] = [
     {
         id: 1,
-        image: "/choose-section-icon1.png",
+        image: "/images/home/choose-section-icon1.png",
         heading: "Learn What Matters Today, Lead What's Next",
         description:
             "Industry-focused content designed to keep you ahead in a fast-changing world",
     },
     {
         id: 2,
-        image: "/choose-section-icon2.png",
+        image: "/images/home/choose-section-icon2.png",
         heading: "Not Just Certificates, Real Transformation",
         description:
             "Build practical skills that open real career opportunities - not just paper achievements",
     },
     {
         id: 3,
-        image: "/choose-section-icon3.png",
+        image: "/images/home/choose-section-icon3.png",
         heading: "Mentorship That Moves You Forward",
         description:
             "Get personal guidance from experienced mentors who care about your progress",
     },
     {
         id: 4,
-        image: "/choose-section-icon4.png",
+        image: "/images/home/choose-section-icon4.png",
         heading: "Built for Dreamers & Doers",
         description:
             "Whether you're starting out or growing further, Skillmyze supports every phase",
