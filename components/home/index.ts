ba060187f72676e3fc16dbbbcbbe4f0c6ea67ef0
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-11 15:29:44 +0530

    Added component file - TrendingCourses

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from './HeroSection';
 export { default as WhyChooseSection } from './WhyChooseSection';
+export { default as TrendingCourses } from './TrendingCourses';
