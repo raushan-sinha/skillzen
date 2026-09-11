71f938cd72189ed22792e45e997869113b6d5b0d
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-11 13:14:18 +0530

    Added Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ResponsiveNavbar } from "@/components/layout";
+import { Footer, ResponsiveNavbar } from "@/components/layout";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -31,6 +31,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
                 <main>
                     {children}
                 </main>
+
+                {/* Footer Component */}
+                <Footer />
             </body>
         </html>
     );
