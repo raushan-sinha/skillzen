8da911eba9bde951f23fec75fd445c6b7dc7c9d7
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-12 21:05:43 +0530

    Added type safety for Trending Course

---
+++ types/trendingCourse.types.ts
@@ -0,0 +1,9 @@
+export interface TrendingCourseProps { 
+    id: number,
+    image: string,
+    heading: string,
+    rating: number,
+    desc: string,
+    duration: string,
+    lectures: number,
+}
