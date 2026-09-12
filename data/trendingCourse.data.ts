b88299501bb7d5a4f95af011c8b61d60c404871d
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-09-12 21:09:23 +0530

    Added data for Trending Courses

---
+++ data/trendingCourse.data.ts
@@ -0,0 +1,63 @@
+import { TrendingCourseProps } from "@/types/trendingCourse.types";
+
+export const trendingCourses: TrendingCourseProps[] = [
+    {
+        id: 1,
+        image: '/images/home/course1.jpg',
+        heading: "Instagram Marketing",
+        desc: "Learn powerful Instagram marketing techniques to grow your audience, increase engagement, and reach potential customers.",
+        rating: 4.3,
+        lectures: 9,
+        duration: "21h 56m",
+    },
+
+    {
+        id: 2,
+        image: '/images/home/course2.jpg',
+        heading: "Canva",
+        desc: "Learn Canva's creative tools to create engaging presentations, graphics, and professional designs for your audience.",
+        rating: 4.3,
+        lectures: 15,
+        duration: "21h 56m",
+    },
+
+    {
+        id: 3,
+        image: '/images/home/course3.jpg',
+        heading: "Communication Skills",
+        desc: "Develop effective communication and soft skills to build stronger relationships and improve your personal and professional growth.",
+        rating: 4.3,
+        lectures: 15,
+        duration: "21h 56m",
+    },
+
+    {
+        id: 4,
+        image: '/images/home/course4.jpg',
+        heading: "Instagram Reels Mastery",
+        desc: "Master Instagram Reels, understand social media algorithms, and learn strategies to increase followers and engagement.",
+        rating: 4.3,
+        lectures: 18,
+        duration: "21h 56m",
+    },
+
+    {
+        id: 5,
+        image: '/images/home/course5.jpg',
+        heading: "Adobe Premiere Pro",
+        desc: "Learn professional video editing techniques and creative workflows to create impactful videos and digital content.",
+        rating: 4.3,
+        lectures: 16,
+        duration: "21h 56m",
+    },
+
+    {
+        id: 6,
+        image: '/images/home/course6.jpg',
+        heading: "Adobe Photoshop",
+        desc: "Explore digital art, photography, and creative design while building practical Photoshop skills for modern projects.",
+        rating: 4.3,
+        lectures: 14,
+        duration: "21h 56m",
+    },
+];
